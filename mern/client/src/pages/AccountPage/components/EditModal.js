daad777657bd9884e8c0bae72aa2c2042c81a8e3
Katherine Langarano <52868773+kathylangarano@users.noreply.github.com>
2023-11-18 00:20:47 -0500

    Update EditModal.js

--- mern/client/src/pages/AccountPage/components/EditModal.js
+++ mern/client/src/pages/AccountPage/components/EditModal.js
@@ -51,13 +51,13 @@ export default function EditModal ({isOpen, close, user}){
     return (
     <Modal show={isOpen} onHide={close}>
         <Modal.Header closeButton>
-            <Modal.Title>Editar mi cuenta</Modal.Title>
+            <Modal.Title>Edit my account</Modal.Title>
         </Modal.Header>
         <Modal.Body>
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group>
-                    <Form.Label>Nombre</Form.Label>
-                    <Form.Control type='text'  placeholder='Escribe un nombre' {...register('name')}/>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type='text'  placeholder='Write a name' {...register('name')}/>
 
                     {errors?.name &&(
                     <Form.Text>
@@ -72,7 +72,7 @@ export default function EditModal ({isOpen, close, user}){
 
                 <Form.Group>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type='email'  placeholder='Escribe un correo electrónico' {...register('email')}/>
+                    <Form.Control type='email'  placeholder='Write an email' {...register('email')}/>
 
                     {errors?.email &&(
                     <Form.Text>
@@ -86,7 +86,7 @@ export default function EditModal ({isOpen, close, user}){
 
 
                 <Form.Group>
-                    <Form.Label>Rol</Form.Label>
+                    <Form.Label>Role</Form.Label>
                     <Form.Control as ='select' disabled={!hasRole('admin')}  {...register('role')}>
                     {Object.keys(roles).map (role =>(<option key={role}>{role}</option>))}
                     
@@ -104,8 +104,8 @@ export default function EditModal ({isOpen, close, user}){
             </Form>
         </Modal.Body>
         <Modal.Footer>
-            <Button variant='secondary' onClick={close}>Cancelar</Button>
-            <Button variant='primary' onClick={handleSubmit(onSubmit)} disabled={!isDirty}>Actualizar mi cuenta </Button>
+            <Button variant='secondary' onClick={close}>Cancel</Button>
+            <Button variant='primary' onClick={handleSubmit(onSubmit)} disabled={!isDirty}>Update my account </Button>
         </Modal.Footer>
     </Modal>
     )
